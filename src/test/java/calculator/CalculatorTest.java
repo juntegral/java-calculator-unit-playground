da9968d3f9e052e72f13c79ca32640dcaf81ca92
PARK JUNSEONG <jun0829park@gmail.com>
2026-03-21 18:24:15 +0900

    feat: 문자열을 잘라서 합하는 메서드에 대한 테스트 코드 추가

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -37,7 +37,7 @@ class CalculatorTest {
         //when
         int actual = calculator.multiply(3,6);
         //then
-        assertEquals(27,actual);
+        assertEquals(18,actual);
     }
     @DisplayName("두 개의 정수를 나눈 결과를 반환할 수 있다.")
     @Test
@@ -49,5 +49,40 @@ class CalculatorTest {
         int actual = calculator.divide(6,3);
         //then
         assertEquals(2,actual);
+    }
+    @DisplayName("기본 구분자(쉼표, 콜론)를 기준으로 분리한 각 숫자의 합을 반환한다.")
+    @Test
+    void testSplitAdd_DefaultDelimiter() {
+        Calculator calculator = new Calculator();
+        int actual = calculator.splitAdd("1,2:3");
+        assertEquals(6, actual);
+    }
+
+    @DisplayName("커스텀 구분자를 지정하여 분리한 각 숫자의 합을 반환한다.")
+    @Test
+    void testSplitAdd_CustomDelimiter() {
+        Calculator calculator = new Calculator();
+        int actual = calculator.splitAdd("//;\n1;2;3");
+        assertEquals(6, actual);
+    }
+
+    @DisplayName("문자열 계산기에 음수를 전달하는 경우 RuntimeException 예외를 발생시킨다.")
+    @Test
+    void testSplitAdd_NegativeNumberException() {
+        Calculator calculator = new Calculator();
+
+        assertThrows(RuntimeException.class, () -> {
+            calculator.splitAdd("-1,2,3");
+        });
+    }
+
+    @DisplayName("문자열 계산기에 숫자 이외의 값을 전달하는 경우 RuntimeException 예외를 발생시킨다.")
+    @Test
+    void testSplitAdd_NotANumberException() {
+        Calculator calculator = new Calculator();
+
+        assertThrows(RuntimeException.class, () -> {
+            calculator.splitAdd("1,a,3");
+        });
     }
 }
